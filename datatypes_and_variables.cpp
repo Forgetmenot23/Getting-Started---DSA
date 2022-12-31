b8b87536ef1e53d8f0bdf86045a213269c3a7f3f
Swati Badola <102805192+Forgetmenot23@users.noreply.github.com>
2022-12-31 15:41:55 +0530

    Add files via upload

---
+++ datatypes_and_variables.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a = 123;
+cout<< a << endl;
+
+char b = 'f';
+cout<< b << endl;
+
+bool bl = true;
+cout<< bl <<endl;
+
+float f = 1.2;
+cout<< f <<endl;
+
+double d = 1.234;
+cout<< d <<endl;
+
+int size = sizeof(b);
+cout<< "Size of a is: " <<size <<endl;
+
+char ch = 98;
+cout<< ch << endl;
+
+char ch1 = 123456;
+cout<< ch1 << endl;
+return 0;
+}
